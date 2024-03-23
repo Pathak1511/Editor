1a4152df335580317d2f1271e6c40ef70a72cb7b
Hritik Pathak <97828866+Pathak1511@users.noreply.github.com>
2024-03-23 17:58:24 +0530

    Update api.js

--- src/hooks/api.js
+++ src/hooks/api.js
@@ -1,6 +1,6 @@
-const BackendAPI2 = "https://confused-pink-chimpanzee.cyclic.app";
+const BackendAPI = "https://confused-pink-chimpanzee.cyclic.app";
 // const BackendAPI = "https://codeflow-3ir4.onrender.com";
-const BackendAPI = "http://localhost:5500";
+const BackendAPI2 = "http://localhost:5500";
 export default BackendAPI;
 
 export const decodeHtml = (html) => {
